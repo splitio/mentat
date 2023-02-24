ba84605dfe024ce10dcc08fcedea4989434cabb6
David Martin <david.martin@split.io>
2023-02-24 08:56:18 -0600

    pull change number from sdk.split.io

--- index.js
+++ index.js
@@ -3,66 +3,111 @@ var axios = require('axios');
 let on_values = [];
 let off_values = [];
 
+const splitName = 'basic';
 const low = -10.0;
 const high = -5.0;
+
 let count = 0;
 let result = NaN;
-
-do {
-	console.log(count++);
-
-	on_values = [];
-	off_value = [];
-
-	for(let i = 0; i < 120; i++) {
-		on_values.push(Math.floor(Math.random() * 100));
-		off_values.push(Math.floor(Math.random() * 100));
-	}
-
-	const on_mean = calculateMean(on_values);
-	const off_mean = calculateMean(off_values);
-
-	result = (on_mean * 100 / off_mean) - 100;
-} while(result > high || result < low);
-
-console.log(calculateMean(on_values));
-console.log(calculateMean(off_values));
-
 let splitStream = [];
+let changeNumber;
 
-for(let i = 0; i < on_values.length; i++) {
-	var uuid = uuidv4();
-	const data = [
-	  {
-	    f: "basic",
-	    i: [
-	      {
-	        k: uuid,
-	        t: Math.random() > 0.5 ? "on" : "off",
-	        m: 1,
-			c: 1676998262981,
-	        r: "default rule"
-	      }
-	    ]
-	  }
-	];
+createImpressionsAndEvents();
 
-	const event = {
-		key: uuid,
-		eventTypeId: 'dbm_error',
-		value: on_values[i],
-		timestamp: new Date().getTime(),
-		properties: { device: "atari" },
-		trafficTypeName: "user",
-	}
+function createImpressionsAndEvents() {
+	do {
+		console.log(count++);
 
-	let entry = {data: data, event: event};
+		on_values = [];
+		off_value = [];
 
-	splitStream.push(entry);
+		for(let i = 0; i < 120; i++) {
+			on_values.push(Math.floor(Math.random() * 100));
+			off_values.push(Math.floor(Math.random() * 100));
+		}
+
+		const on_mean = calculateMean(on_values);
+		const off_mean = calculateMean(off_values);
+
+		result = (on_mean * 100 / off_mean) - 100;
+	} while(result > high || result < low);
+
+	console.log(calculateMean(on_values));
+	console.log(calculateMean(off_values));
+
+	changeNumber = getChangeNumber(splitName);
+	changeNumber.then(function (value) {
+		changeNumber = value;
+		buildImpressionsAndEvents();
+		sendImpressions(splitStream);
+		sendEvents(splitStream);
+	}, function (error) {
+		console.log(error);	
+	});
 }
 
-sendImpressions(splitStream);
-sendEvents(splitStream);
+function buildImpressionsAndEvents() {
+	console.log('buildImpressionsAndEvents!')
+
+	for(let i = 0; i < on_values.length; i++) {
+		var uuid = uuidv4();
+		const data = [
+		  {
+		    f: splitName,
+		    i: [
+		      {
+		        k: uuid,
+		        t: Math.random() > 0.5 ? "on" : "off",
+		        m: 1,
+				c: changeNumber,
+		        r: "default rule"
+		      }
+		    ]
+		  }
+		];
+
+		const event = {
+			key: uuid,
+			eventTypeId: 'dbm_error',
+			value: on_values[i],
+			timestamp: new Date().getTime(),
+			properties: { device: "atari" },
+			trafficTypeName: "user",
+		}
+
+		let entry = {data: data, event: event};
+
+		splitStream.push(entry);
+	}	
+}
+
+async function getChangeNumber(splitName) {
+	let result = 1;
+
+	var config = {
+	  method: 'get',
+	  url: ' https://sdk.split.io/api/splitChanges?since=-1',
+	  headers: { 
+	    'Authorization': 'Bearer 70jiipvg64kkvds3ar48sf0ugrptgvpqts8d', 
+	    'Content-Type': 'application/json'
+	  }
+	};		
+
+	// console.log(eventConfig);
+	await axios(config)
+	.then(function (response) {
+	  for(const split of response.data.splits) {
+	  	if(split.name === splitName) {
+	  		result = split.changeNumber;
+	  	}
+	  }
+	})
+	.catch(function(error) {
+	  console.log(error);
+	});	
+
+	return result;
+}
 
 function sendImpressions(splitStream) {
 	let itr = 0;	
@@ -73,10 +118,10 @@ function sendImpressions(splitStream) {
 		const delta = 1000 * 60 * 60 * 1;
 
 		const ts = new Date().getTime() - interval - (delta * itr++);
-		// console.log(JSON.stringify(data, 0, 2));
 		data[0].i[0].m = ts; 
 		console.log(new Date(ts));
-
+		console.log(JSON.stringify(data, 0, 2));
+		
 		var config = {
 		  method: 'post',
 		  url: 'https://events.split.io/api/testImpressions/bulk',
