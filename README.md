189c335ff1df32b129e1ef6904cc2ed68c68ea5a
David Martin <david.martin@split.io>
2023-03-31 11:53:47 -0500

    command line questioning

--- README.md
+++ README.md
@@ -6,11 +6,21 @@ By "backdating" the impressions and events, a Split user can generate charts now
 
 In order for the approach to work currently...
 
- - You must copy-and-paste the version number of your target split.  This turns out to be tricky to find.  I can help.  david.martin@split.io
+ - You must enter the version number of your target split.  The split must define a 50/50 default rule.  For best results, the version of the split should be at least three days old.   
 
- - Your target split must have a version starting for at least three days in the past.
+ - You must enter your server-sider SDK API key
 
-The target split should be a 50/50 rollout.
+The split name and SDK key are cached in a config.json for convenience doing repeat runs.
 
+ - Enter the desired impact.  This is very approximate.
 
+ - Enter the desired event type id
 
+This will be the name of the event that shows up with a run.  You will need to create a metric, and maybe relax the experiment settings (I use 0.2 and sample size of 10).
+
+For extra credit, setup dimensional analysis:
+
+ - "platform" is the dimension name
+ - "chrome", "edge", "firefox", "android", and "ios" are values, all lower case.
+
+Author: david.martin@split.io
--- index.js
+++ index.js
@@ -1,32 +1,122 @@
-var axios = require('axios');
+const axios = require('axios');
 const fs = require('fs');
+const readlineSync = require('readline-sync');
 
 let on_values = [];
 let off_values = [];
 
-const splitName = 'basic';
-const low = -10.0;
-const high = -5.0;
+let config;
+if(!fs.existsSync('config.json')) {
+	config = {
+		splitName: null,
+		splitServerApiKey: null,
+		impact: null,
+		eventTypeId: null
+	}
+} else {
+	const configJsonFile = fs.readFileSync('config.json');
+	config = JSON.parse(configJsonFile);
+}
+
+let splitName = '';
+if(config.splitName) {
+	splitName = config.splitName;
+}
+
+let answer = readlineSync.question('split name [' + splitName + ']? ');
+if(answer !== '') {
+	splitName = answer;
+}
+
+let splitServerApiKey = '';
+if(config.splitServerApiKey) {
+	splitServerApiKey = config.splitServerApiKey;
+}
+
+answer = readlineSync.question('split server api key [' + splitServerApiKey + ']? ');
+if(answer !== '') {
+	splitServerApiKey = answer;
+}
+
+let impact = 0;
+if(config.impact) {
+	impact = config.impact;
+}
+
+answer = readlineSync.question('desired impact -50 to +50 [' + impact + ']? ');
+if(answer !== '') {
+	answer = parseInt(answer, 10);
+}
+
+let eventTypeId;
+if(config.eventTypeId) {
+	eventTypeId = config.eventTypeId;
+}
+
+answer = readlineSync.question('eventTypeId [' + eventTypeId + ']? ');
+if(answer !== '') {
+	eventTypeId = answer;
+}
+
+config.splitName = splitName;
+config.splitServerApiKey = splitServerApiKey;
+config.impact = impact;
+config.eventTypeId = eventTypeId;
+
+fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+
+const low = Math.min;
+const high = Math.max;
 
 let count = 0;
 let result = NaN;
 let splitStream = [];
 let changeNumber;
 
-const splitServerApiKey = fs.readFileSync('SPLIT_SERVER_API_KEY', 'utf8').trim();
 
 createImpressionsAndEvents();
+exportToCsv();
+
+function exportToCsv() {
+	for(let i = 0; i < on_values.length; i++) {	
+		fs.appendFileSync('export.csv', '' + i + ',' + on_values[i] + ',' + off_values[i] + '\n');
+	}
+}
 
 function createImpressionsAndEvents() {
+
+	const c = (6 * impact) - 200;
+
+	// const c = 100; // 53.56%
+	// const c = 0; // 34.32%
+	// const c = -100; // 9.45%
+	// const c = -130; // inconclusive
+	// const c = -225; // -14.93%
+	// const c = -325; // -23.44%
+  // const c = -500; // -47.51%
 	do {
-		console.log(count++);
+		// console.log(count++);
 
 		on_values = [];
 		off_value = [];
 
-		for(let i = 0; i < 120; i++) {
-			on_values.push(Math.floor(Math.random() * 100));
-			off_values.push(Math.floor(Math.random() * 100));
+		const limit = 1000;
+		for(let i = 0; i < limit; i++) {
+
+			const right = Math.cos(i*4 * (Math.PI / 180)) * 1000;
+			const left = (3 * right) / 4;
+			
+			let on_value = Math.max(c + right + (Math.random() * 100), 0);
+			if(on_value == 0) {
+				on_value = Math.random() * 100;
+			}
+
+			on_values.push(on_value);
+			let off_value = Math.max(left+(Math.random() * 100), 0);
+			if(off_value == 0) {
+				off_value = Math.random() * 100;
+			}
+			off_values.push(off_value);
 		}
 
 		const on_mean = calculateMean(on_values);
@@ -35,8 +125,9 @@ function createImpressionsAndEvents() {
 		result = (on_mean * 100 / off_mean) - 100;
 	} while(result > high || result < low);
 
-	console.log(calculateMean(on_values));
-	console.log(calculateMean(off_values));
+	// console.log(calculateMean(on_values));
+	// console.log(calculateMean(off_values));
+	// console.log(result);
 
 	changeNumber = getChangeNumber(splitName);
 	changeNumber.then(function (value) {
@@ -44,6 +135,7 @@ function createImpressionsAndEvents() {
 		buildImpressionsAndEvents();
 		sendImpressions(splitStream);
 		sendEvents(splitStream);
+		console.log('finished');		
 	}, function (error) {
 		console.log(error);	
 	});
@@ -53,6 +145,7 @@ function buildImpressionsAndEvents() {
 	console.log('buildImpressionsAndEvents!')
 
 	for(let i = 0; i < on_values.length; i++) {
+		const treatment = Math.random() > 0.5 ? "on" : "off"
 		var uuid = uuidv4();
 		const data = [
 		  {
@@ -60,22 +153,29 @@ function buildImpressionsAndEvents() {
 		    i: [
 		      {
 		        k: uuid,
-		        t: Math.random() > 0.5 ? "on" : "off",
+		        t: treatment,
 		        m: 1,
-				c: changeNumber,
+						c: changeNumber,
 		        r: "default rule"
 		      }
 		    ]
 		  }
 		];
 
+		const dimensions = ['chrome', 'edge', 'firefox', 'android', 'ios'];
+		const i = Math.floor(Math.random() * 100) % dimensions.length;
+
 		const event = {
 			key: uuid,
-			eventTypeId: 'dbm_error',
-			value: on_values[i],
+			eventTypeId: eventTypeId,
+			value: treatment === 'on' ? on_values[i] : off_values[i],
 			timestamp: new Date().getTime(),
-			properties: { device: "atari" },
+			properties: { platform: dimensions[i] },
 			trafficTypeName: "user",
+		}
+
+		if(dimensions[i] === 'ios') {
+			event.value = event.value + 150;
 		}
 
 		let entry = {data: data, event: event};
@@ -89,6 +189,7 @@ async function getChangeNumber(splitName) {
 
 	var config = {
 	  method: 'get',
+	  // url: ' https://sdk.split.io/api/splitChanges?since=-1',
 	  url: ' https://sdk.split.io/api/splitChanges?since=-1',
 	  headers: { 
 	    'Authorization': 'Bearer ' + splitServerApiKey,
@@ -112,21 +213,32 @@ async function getChangeNumber(splitName) {
 	return result;
 }
 
+const interval = 1000 * 60 * 60 * 24 * 1;
+const delta = 1000 * 60 * 20;
+
+function getTimestamp(itr) {
+	return new Date().getTime() - interval - (delta * itr++);
+}
+
 function sendImpressions(splitStream) {
 	let itr = 0;	
+	let error = false;
 	for(const entry of splitStream) {
 		let data = entry.data;
 
-		let interval = 1000 * 60 * 60 * 24 * 1;
-		const delta = 1000 * 60 * 60 * 1;
+		// backdating
+		const ts = getTimestamp(itr++);
 
-		const ts = new Date().getTime() - interval - (delta * itr++);
+		// forwardating
+		// const ts = new Date().getTime() + interval + (delta * itr++);
+		
 		data[0].i[0].m = ts; 
 		console.log(new Date(ts));
-		console.log(JSON.stringify(data, 0, 2));
+		// console.log(JSON.stringify(data, 0, 2));
 		
 		var config = {
 		  method: 'post',
+		  // url: 'https://events.split.io/api/testImpressions/bulk',
 		  url: 'https://events.split.io/api/testImpressions/bulk',
 		  headers: { 
 		    'Authorization': 'Bearer ' + splitServerApiKey, 
@@ -137,28 +249,35 @@ function sendImpressions(splitStream) {
 		// console.log(config);
 		axios(config)
 		.then(function (response) {
-			console.log('o');
+			// console.log('o');
 			// console.log(JSON.stringify(response.data));
 		})
 		.catch(function (error) {
 		  console.log(error);
-		  return;
+		  error = true;
 		});
+
+		if(error) {
+			return;
+		}
 	}
 }
 
 function sendEvents(splitStream) {
 	let itr = 0;
+	let error = false;
 
 	for(const entry of splitStream) {
 		let event = entry.event;
 
-		let interval = 1000 * 60 * 60 * 24 * 1;
-		const delta = 1000 * 60 * 60 * 1;
+		// backdating
+		const ts = getTimestamp(itr++);
 
-		const ts = new Date().getTime() - interval - (delta * itr++);	
+		// forwardating
+		// const ts = new Date().getTime() + interval + (delta * itr++);
+		
 		event.timestamp = ts;
-		console.log(new Date(ts));
+		// console.log(new Date(ts));
 
 		var eventConfig = {
 		  method: 'post',
@@ -174,15 +293,19 @@ function sendEvents(splitStream) {
 		axios(eventConfig)
 		.then(function (response) {
 		  // console.log(JSON.stringify(response.data));
-			console.log('+');
+			// console.log('+');
 		})
 		.catch(function(error) {
 		  console.log(error);
-		  return;
+		  error = true;
 		});		
+		if(error) {
+			return;
+		}
 
 		setTimeout(() => {}, 500);
 	}
+
 }
 
 function uuidv4() {
